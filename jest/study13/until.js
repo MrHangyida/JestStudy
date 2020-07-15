0c506457a48ae4d6c0221e36f0f529b8f498996b
hydls <1921135733@qq.com>
2020-07-15 19:42:52 +0800

    mockStudy End

---
+++ jest/study13/until.js
@@ -0,0 +1,15 @@
+export default class Until {
+    constructor() {
+        this.number = 0
+    }
+    init() {
+
+    }
+    a(x, y) {
+        this.number = x * y
+        return x * y
+    }
+    b() {
+
+    }
+}
---
+++ jest/study13/until.test.js
@@ -0,0 +1,21 @@
+import Until from './until'
+
+describe('until 模块', () => {
+    let until = null
+    beforeAll(() => {
+        console.log('beforeAll')
+    })
+    beforeEach(() => {
+        until = new Until()
+    })
+    afterEach(() => {
+        console.log('afterEach')
+    })
+    afterAll(() => {
+        console.log('afterAll')
+    })
+    console.log(until, 1111)
+    test('测试 a方法', () => {
+        expect(until.a(2, 3)).toBe(6)
+    })
+})
